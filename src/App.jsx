75fdff50d0fe1baff4bf2fdd6d2c08c4982c91fa
mykewayner <mykewayner@gmail.com>
2022-07-01 02:40:23 -0300

    Feat: created router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './pages/Home/index.jsx'
+import { Success } from './pages/Success/index.jsx'
 
-const App = () => {
-  return <div>My App</div>;
-};
+function App() {
+  //const [count, setCount] = useState(0)
 
-export default App;
+  return( <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="success" element={<Success />} />
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
