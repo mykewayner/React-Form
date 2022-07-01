4ae2ecaeb6408f8ed61b3751334d5a1dc42434ca
mykewayner <mykewayner@gmail.com>
2022-07-01 02:09:29 -0300

    Feat: created button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { ButtonStyle } from './styles'
+
+export function Button({ value }) {
+
+    return(
+        
+  
+        <ButtonStyle value={value}>{value}</ButtonStyle>
+   
+    )
+
+
+
+}
+
+
+export default Button;
